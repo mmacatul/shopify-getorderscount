1e9bfd5c6b1eb05a1813e6b4b33a5d34a32cbf54
mmacatul <mmacatul@users.noreply.github.com>
2016-11-21 00:52:09 -0800

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get('/GetOrdersCount', function (req, res) {
     res.end();
 });
 
-http.listen(8080, '52.90.255.27', function () {
+http.listen(8080, '127.0.0.1', function () {
     console.log('listening on *:8080');
 });
