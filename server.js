a1af718aa4745fe70c142784f105916d3497c923
michael macatula <mmacatul@sbcglobal.net>
2016-11-20 21:51:13 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+//var express = require('express');
+//var app = express();
+//var fs = require("fs");
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/GetOrdersCount', function (req, res) {
+    
+    res.writeHead(301, {
+        'Location': 'https://08bdb249b92b67b005959abaecc386d4:42f34ed5b53d0b0bd0292fdb9a463233@questnutrition.myshopify.com/admin/orders/count.json?financial_status=authorized'
+    });
+
+    res.end();
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
