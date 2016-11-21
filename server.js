4026a2ba6d00e12894b56e1f5bbe1a66d18289c9
mmacatul <mmacatul@users.noreply.github.com>
2016-11-21 01:09:07 -0800

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.get('/GetOrdersCount', function (req, res) {
     res.end();
 });
 
-http.listen(8080, '127.0.0.1', function () {
+var ip =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+http.listen(port, ip, function () {
     console.log('listening on *:8080');
 });
